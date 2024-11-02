9bc8ef2419fe7840aa169d640508169897753432
n1sp <n1sp.pc001@gmail.com>
2024-11-03 01:03:39 +0900

    マッピング設定を修正

--- src/main/java/com/n1sp/springbootdemo/controller/HelloApplicationController.java
+++ src/main/java/com/n1sp/springbootdemo/controller/HelloApplicationController.java
@@ -1,11 +1,13 @@
 package com.n1sp.springbootdemo.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@RequestMapping("/api/helloWorld")
 public class HelloApplicationController {
-    @RequestMapping("/")
-    public String test() {
+    @GetMapping
+    public String helloWorld() {
         return "Hello World";
     }
 }
